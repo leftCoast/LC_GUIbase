ce5d199c05aef1c478875715bb79bcb0ee39048d
Jim Lee <gitHub@leftcoast.biz>
2022-01-12 20:55:46 -0800

    Update eventMgr.h
    We are using DRAG_TIME for both time to switch between touch to drag AND time between drag events. Need to break this into two timers.

--- eventMgr.h
+++ eventMgr.h
@@ -6,7 +6,7 @@
 #include <timeObj.h>
 #include <baseGraphics.h>
 
-#define	DRAG_TIME	500	// How many milliseconds before it goes from a click to a drag?
+#define	DRAG_TIME	150	// How many milliseconds before it goes from a click to a drag?
 #define	DRAG_DIST	10		// If the finger moves further than this, we'll call it a drag.
 // ***************************************************************
 // Here we have the management of screen actions. Get the touches
